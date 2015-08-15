def0c116940736003f8c42ef8fc82a05d13b27d4
Paulina Nogal <nogalpaulina@gmail.com>
2015-08-14 21:16:09 -0400

    call yelp API once and store locations

--- js/map.js
+++ js/map.js
@@ -1,50 +1,54 @@
 /* Neighborhood map application
  * Created by Paulina Nogal
  */
-
 function initialize() {
-	// Searching with an empty string returns popular points of interest
-	Model.getYelpLocations('');
 	// Initialize map
 	Map.initialize();
-};
+	// Load locations from Yelp
+	Model.getYelpLocations();
+}
 
 var Model = {
-	getYelpLocations: function(term) {
-		var yelpURL = 'http://api.yelp.com/business_review_search?lat=41.48611&long=-71.3247682&term=' + term + '&radius=3&num_biz_requested=11&ywsid=glDt6TNTp1kRn1fL055uNQ&callback=?';
+	locations: [],
+	getYelpLocations: function() {
+		var yelpURL = 'http://api.yelp.com/business_review_search?lat=41.48611&long=-71.3247682&radius=3&num_biz_requested=11&ywsid=glDt6TNTp1kRn1fL055uNQ&callback=?';
 		var that = this;
 		$.ajax({
-			url: yelpURL, 
-			async: false,
+			url: yelpURL,
 			dataType: "jsonp",
 			success: function(response) {
-				if (response.businesses.length > 0) {  
+				if (response.businesses.length > 0) {
 					for (i = 0; i < response.businesses.length; i++) {
 						// Build a location object
 						var location = {};
 						var business = response.businesses[i];
-			    			location.title = business.name;
-			    			var address = business.address1 + " " + business.city + ", " + business.state;
-			    			location.description = address;
-			    			addYelpMarker(location);
-			    		}  
+						location.title = business.name;
+						var address = business.address1 + " " + business.city + ", " + business.state;
+						location.description = address;
+						addYelpLocation(location);
+					}
 				} else {
 					ViewModel.sidebarList('<h4>No results found</h4>');
 				}
 			}
-		});		
+		});
 	}
 };
 
-function addYelpMarker(location) {
+function addYelpLocation(location) {
 	// Convert the location's address to lat and long for map marker
 	var geocoder = new google.maps.Geocoder();
-	geocoder.geocode( { 'address': location.description}, function(results, status) {
+	geocoder.geocode({
+		'address': location.description
+	}, function(results, status) {
 		if (status === google.maps.GeocoderStatus.OK) {
 			var position = results[0].geometry.location;
 			location.latLng = [position.G, position.K];
+			// Store the location in the model
+			Model.locations.push(location);
+			// Add the location marker to the map
 			Map.addMarkers([location]);
-		} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) { 
+		} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
 			// Fail silently if the location can't be converted
 		}
 	});
@@ -53,15 +57,23 @@ function addYelpMarker(location) {
 // Set up knockout bindings
 var ViewModel = {
 	sidebarList: ko.observable(),
-	searchTerm: ko.observable(),
+	searchTerm: ko.observable(''),
 	searchButton: function() {
 		var self = this;
 		// Remove existing markers from the map
 		Map.removeMarkers();
 		// Clear sidebar list
-		this.clearSidebar();
-		// Get locations based on search term
-		Model.getYelpLocations(self.searchTerm());
+		self.clearSidebar();
+		// Filter locations based on search term
+		var searchLocations = Model.locations.filter(function(location) {
+			return location.title.toLowerCase().indexOf(self.searchTerm().toLowerCase()) > -1 ||
+				location.description.toLowerCase().indexOf(self.searchTerm().toLowerCase()) > -1;
+		});
+		if (searchLocations.length < 1) {
+			ViewModel.sidebarList('<h4>No results found</h4>');
+		} else {
+			Map.addMarkers(searchLocations);
+		}
 	},
 	clearSidebar: function() {
 		this.sidebarList('');
@@ -90,7 +102,7 @@ var Map = {
 		this.defaultIcon = 'img/black-marker.png';
 		this.activeIcon = 'img/yellow-marker.png';
 		this.infoWindow = new google.maps.InfoWindow();
-		// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+		// Override our map zoom level once our fitBounds function runs
 		var boundsListener = google.maps.event.addListener((this.map), 'bounds_changed', function(event) {
 			this.setZoom(14);
 			google.maps.event.removeListener(boundsListener);
@@ -110,9 +122,11 @@ var Map = {
 				map: this.map,
 				title: locations[i].title
 			});
-			setTimeout(function() {
-				marker.setAnimation(null)
-			}, 750);
+			(function(marker) {
+				return setTimeout(function() {
+					marker.setAnimation(null);
+				}, 750);
+			}(marker));
 			this.markers.push(marker);
 			this.sidebarList += "<h5 onclick='Map.clickMarker(" + (this.markers.length - 1) + ");'>" + locations[i].title + "</h5>";
 			// Display multiple markers on a map
@@ -121,15 +135,15 @@ var Map = {
 			// Allow each marker to have an info window
 			google.maps.event.addListener(marker, 'click', (function(marker, location, that) {
 				return function() {
-					for (var i=0; i<that.markers.length; i++) {
-                   				that.markers[i].setIcon(that.defaultIcon);
-                			}
+					for (var i = 0; i < that.markers.length; i++) {
+						that.markers[i].setIcon(that.defaultIcon);
+					}
 					marker.setIcon(that.activeIcon);
 					that.infoWindow.close();
 					this.map.panTo(marker.getPosition());
 					marker.setAnimation(google.maps.Animation.BOUNCE);
 					setTimeout(function() {
-						marker.setAnimation(null)
+						marker.setAnimation(null);
 					}, 750);
 					that.infoWindow.setContent(infoWindowContent(location.title, location.description));
 					that.infoWindow.open(this.map, marker);
@@ -142,7 +156,7 @@ var Map = {
 		var infoWindowContent = function(title, description) {
 			return '<div class="info_content">' +
 				'<h3>' + title + '</h3>' +
-				'<p>' + description + '</p>' + 
+				'<p>' + description + '</p>' +
 				'</div>';
 		};
 	},
@@ -155,10 +169,10 @@ var Map = {
 		}
 		this.markers = [];
 	}
-}
+};
 
 $('#input').keyup(function(e) {
-	if(e.which == 13) { // Enter key pressed
+	if (e.which == 13) { // Enter key pressed
 		$('#search').click(); // Trigger search button click event
 	}
 });
